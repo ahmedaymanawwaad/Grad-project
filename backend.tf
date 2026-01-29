1304ae38a9f6c34a0ef2523d611bd0b0b3004dd7
root <root@localhost.localdomain>
2026-01-29 11:57:37 +0200

    add backend file for terraform

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "grad-proj-nti"       # S3 bucket name
-    key            = "terraform.tfstate"   # path inside the bucket for state
-    region         = "us-east-1"           # region of the bucket
-    dynamodb_table = "grad-proj-nti"       # DynamoDB table for state locking
-    encrypt        = true                  # enable encryption
+    bucket         = "grad-proj-nti"       
+    key            = "terraform.tfstate"   
+    region         = "us-east-1"           
+    dynamodb_table = "grad-proj-nti"       
+    encrypt        = true                 
   }
 }
